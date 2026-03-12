2debb2df58d069960d3eaa62db1bedcfb283d746
Karl Nina <ninkarl@gmail.com>
2026-03-11 20:08:35 -0400

    Refactor la clase Computadora y su Builder para mejorar la construcción de instancias y eliminar la interfaz IComputadoraBuilder
    - builder anidado

--- Builder/Program.cs
+++ Builder/Program.cs
@@ -8,14 +8,38 @@ Console.WriteLine("Construyendo una computadora para servidor...");
 // var computadoraServidor = servidorBuilder.ObtenerComputadora();
 // computadoraServidor.MostrarEspecificaciones();
 
+// var director = new Director();
+// var computadoraServidor = director.ConstruirServidor(new ServidorBuilder());
+// computadoraServidor.MostrarEspecificaciones();
+
+// try
+// {
+//     Console.WriteLine("\nConstruyendo una computadora de escritorio...");
+//     var computadoraEscritorio = director.ConstruirComputadora(new EscritorioBuilder());
+//     computadoraEscritorio.MostrarEspecificaciones();
+// }
+// catch (InvalidOperationException ex)
+// {
+//     Console.WriteLine($"Error: {ex.Message}");
+// }
+
+// var computadora = new Computadora.Builder();
+// computadora.ConfigurarCPU("Intel Core i9-11900K");
+// // computadora.ConfigurarRAM("32GB");
+// computadora.ConfigurarAlmacenamiento("1TB SSD");
+// computadora.ConfigurarEsGamer(true);
+// Computadora computadoraPersonalizada = computadora.BuilderComputadora();
+// computadoraPersonalizada.MostrarEspecificaciones();
+
+
 var director = new Director();
-var computadoraServidor = director.ConstruirServidor(new ServidorBuilder());
+var computadoraServidor = director.ConstruirServidor(new Computadora.Builder());
 computadoraServidor.MostrarEspecificaciones();
 
 try
 {
     Console.WriteLine("\nConstruyendo una computadora de escritorio...");
-    var computadoraEscritorio = director.ConstruirComputadora(new EscritorioBuilder());
+    var computadoraEscritorio = director.ConstruirComputadora(new Computadora.Builder());
     computadoraEscritorio.MostrarEspecificaciones();
 }
 catch (InvalidOperationException ex)
@@ -31,6 +55,41 @@ public class Computadora
     public string Almacenamiento { get; set; } = "HDD";
     public bool EsGamer { get; set; }
 
+    public class Builder
+    {
+        private Computadora computadora = new Computadora();
+
+        public void ConfigurarCPU(string cpu)
+        {
+            computadora.CPU = cpu;
+        }
+
+        public void ConfigurarRAM(string ram)
+        {
+            computadora.RAM = ram;
+        }
+
+        public void ConfigurarAlmacenamiento(string almacenamiento)
+        {
+            computadora.Almacenamiento = almacenamiento;
+        }
+
+        public void ConfigurarEsGamer(bool esGamer)
+        {
+            computadora.EsGamer = esGamer;
+        }
+
+        public Computadora BuilderComputadora()
+        {
+            if (string.IsNullOrEmpty(computadora.RAM))
+            {
+                throw new InvalidOperationException("La RAM es un campo obligatorio.");
+            }
+
+            return computadora;
+        }
+    }
+
     public void MostrarEspecificaciones()
     {
         Console.WriteLine($"CPU: {CPU}");
@@ -40,79 +99,22 @@ public class Computadora
     }
 }
 
-public interface IComputadoraBuilder
-{
-    void ConfigurarCPU(string cpu);
-    void ConfigurarRAM(string ram);
-    void ConfigurarAlmacenamiento(string almacenamiento);
-    void ConfigurarEsGamer(bool esGamer);
-    Computadora ObtenerComputadora();
-}
-
-public class ServidorBuilder : IComputadoraBuilder
-{
-    private Computadora _computadora = new Computadora();
-
-    public void ConfigurarCPU(string cpu) => _computadora.CPU = cpu;
-
-    public void ConfigurarRAM(string ram) => _computadora.RAM = ram;
-
-    public void ConfigurarAlmacenamiento(string almacenamiento) => _computadora.Almacenamiento = almacenamiento;
-
-    public void ConfigurarEsGamer(bool esGamer)
-    {
-        _computadora.EsGamer = esGamer;
-    }
-
-    public Computadora ObtenerComputadora()
-    {
-        return _computadora;
-    }
-}
-
-public class EscritorioBuilder : IComputadoraBuilder
-{
-    private Computadora _computadora = new Computadora();
-
-    public void ConfigurarCPU(string cpu) => _computadora.CPU = cpu;
-
-    public void ConfigurarRAM(string ram) => _computadora.RAM = ram;
-
-    public void ConfigurarAlmacenamiento(string almacenamiento) => _computadora.Almacenamiento = almacenamiento;
-
-    public void ConfigurarEsGamer(bool esGamer)
-    {
-        _computadora.EsGamer = esGamer;
-    }
-
-    public Computadora ObtenerComputadora()
-    {
-        if (string.IsNullOrEmpty(_computadora.CPU) || _computadora.CPU == "Generico")
-        {
-            throw new InvalidOperationException("La CPU debe ser configurada antes de obtener la computadora.");
-        }
-
-        return _computadora;
-    }
-}
-
 
 public class Director
 {
-    public Computadora ConstruirComputadora(IComputadoraBuilder builder)
+    public Computadora ConstruirComputadora(Computadora.Builder builder)
     {
-        builder.ConfigurarRAM("32GB");
         builder.ConfigurarAlmacenamiento("1TB SSD");
         builder.ConfigurarEsGamer(true);
-        return builder.ObtenerComputadora();
+        return builder.BuilderComputadora();
     }
 
-    public Computadora ConstruirServidor(IComputadoraBuilder builder)
+    public Computadora ConstruirServidor(Computadora.Builder builder)
     {
         builder.ConfigurarCPU("AMD EPYC 7742");
         builder.ConfigurarRAM("512GB");
         builder.ConfigurarAlmacenamiento("2TB SSD");
         builder.ConfigurarEsGamer(false);
-        return builder.ObtenerComputadora();
+        return builder.BuilderComputadora();
     }
 }
