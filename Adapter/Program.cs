6c742023e8a7f1f6304d3e4d0399072c444c4811
Karl Nina <ninkarl@gmail.com>
2026-04-01 22:41:29 -0400

    Agregar implementación de los decoradores Log y Encriptación en el patrón Notificador
    - colaboracion entre: adapte + decorador

--- Adapter/Program.cs
+++ Adapter/Program.cs
@@ -1,11 +1,22 @@
 ﻿// Ejemplo de patrón Adapter en C#
-var sistemaLegacy = new SistemaLegacy();
-INotificador adaptador = new AlertaAdapter(sistemaLegacy);
-adaptador.Enviar("¡Alerta importante!");
+// var sistemaLegacy = new SistemaLegacy();
+// INotificador adaptador = new AlertaAdapter(sistemaLegacy);
+// adaptador.Enviar("¡Alerta importante!");
 
-var servicioEmailAntiguo = new ServicioEmailAntiguo();
-INotificador emailAdaptador = new EmailAdapter(servicioEmailAntiguo);
-emailAdaptador.Enviar("¡Email importante! desde el adaptador! nuevo ejemplo");
+// var servicioEmailAntiguo = new ServicioEmailAntiguo();
+// INotificador emailAdaptador = new EmailAdapter(servicioEmailAntiguo);
+// emailAdaptador.Enviar("¡Email importante! desde el adaptador! nuevo ejemplo");
+
+
+// Colaboración entre patrones: Adapter + Decorator
+INotificador notificador = new EncriptacionDecorador(
+    new LogNotificadorDecorador(
+        new NotificadorBase()
+    )
+);
+notificador.Enviar("Enviado mensaje desde un nivel de decorador!");
+
+
 
 // --------------------------------------------------------------
 
@@ -77,3 +88,59 @@ public class EmailAdapter : INotificador
     }
 }
 
+// 2. Componente base
+public class NotificadorBase : INotificador
+{
+    public void Enviar(string mensaje)
+    {
+        Console.WriteLine($"Enviando mensaje: {mensaje}");
+    }
+}
+
+// 3. Decorador base(abstracto)
+public abstract class NotificadorDecorador : INotificador
+{
+    protected INotificador _envoltorio;
+
+    public NotificadorDecorador(INotificador envoltorio)
+    {
+        _envoltorio = envoltorio;
+    }
+
+    public virtual void Enviar(string mensaje)
+    {
+        _envoltorio.Enviar(mensaje);
+    }
+}
+
+// 4. Decorador concreto: Log
+public class LogNotificadorDecorador : NotificadorDecorador
+{
+    public LogNotificadorDecorador(INotificador envoltorio) : base(envoltorio) { }
+
+    public override void Enviar(string mensaje)
+    {
+        Console.WriteLine($"[LOG] Enviando mensaje: {mensaje}");
+        base.Enviar(mensaje);
+    }
+}
+
+// 5. Decorador concreto: Encriptacion
+public class EncriptacionDecorador : NotificadorDecorador
+{
+    public EncriptacionDecorador(INotificador envoltorio) : base(envoltorio) { }
+
+    public override void Enviar(string mensaje)
+    {
+        string mensajeEncriptado = Encriptar(mensaje);
+        base.Enviar(mensajeEncriptado);
+    }
+
+    private string Encriptar(string mensaje)
+    {
+        // Lógica de encriptación (ejemplo simple)
+        char[] caracteres = mensaje.ToCharArray();
+        Array.Reverse(caracteres);
+        return new string(caracteres);
+    }
+}
